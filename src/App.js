6c897ec68ad7afecb412471c5b8a7a49983468d5
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-02-14 23:34:17 +0400

    completed structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div>
+      <h1>Welcome to the Counter App</h1>
+      <h4>The Counter is {counter}</h4>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
