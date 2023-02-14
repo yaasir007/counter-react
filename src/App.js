a99970f885ff29dfe4c58fb063f391dadf0ff71d
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-02-14 20:45:33 +0400

    hard reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [ counter, setCounter ] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+
+  const decrement = () => {
+    setCounter((counter - 1));
+  };
+
   return (
-    <div className="App">
-      
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>The Counter is {counter}</span>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
