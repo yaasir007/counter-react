061e93b31a54f07907498492c49f9f3ab231c493
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-02-16 22:15:21 +0400

    working on frotns

--- src/App.js
+++ src/App.js
@@ -13,13 +13,20 @@ function App() {
   };
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Welcome to the Counter App
-      </h1>
-      <h4>The Counter is {counter}</h4>
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
+    <div className="flex justify-center items-center h-screen flex-col bg-slate-800	text-slate-300">
+      <h1 className="text-5xl font-bold mb-10">Welcome to the Counter App</h1>
+      <h4 className="text-4xl">
+        The Counter is{" "}
+        <span className="border border-slate-100 pt-3 pb-3 pl-6 pr-6">{counter}</span>
+      </h4>
+      <div className="flex text-5xl mt-10">
+        <button onClick={decrement} className="p-7 border border-slate-100">
+          -
+        </button>
+        <button onClick={increment} className="p-5 border border-slate-100">
+          +
+        </button>
+      </div>
     </div>
   );
 }
